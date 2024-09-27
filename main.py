d24e0ac441afcba64641990d1a23f05ed3cfd5ea
unknown <foreverblessedmicaiah@gmail.com>
2024-09-27 21:06:42 +0845

    this is the initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils import randomize_names
+
+if __name__ == "__main__":
+    print(__name__)
+    print(randomize_names())
---
+++ utils.py
@@ -0,0 +1,7 @@
+import random
+
+names = ["rukky","emmanuel", "micaiah"  ]
+
+def randomize_names():
+    random.shuffle(names)
+    return names
